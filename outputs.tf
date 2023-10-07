475fbf65b705d5e0cd1442671a33efaf42042e5c
Adalbert Barta <adalbertbarta@gmail.com>
2023-10-07 10:27:24 +0200

    local file output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,23 @@
+data "local_file" "private_key_pem" {
+  depends_on = [null_resource.save_keys]
+  filename = "${path.module}/key"
+}
+
 output "private_key_pem" {
+  depends_on = [null_resource.save_keys]
+  description = "The private key ready."
+  value       = "${path.module}/key"
   sensitive = true
-  value = tls_private_key.this.private_key_pem
+}
+
+data "local_file" "public_key_openssh" {
+  depends_on = [null_resource.save_keys]
+  filename = "${path.module}/key.pub"
 }
 
 output "public_key_openssh" {
+  depends_on = [null_resource.save_keys]
+  description = "The public key ready."
+  value       = "${path.module}/key.pub"
   sensitive = true
-  value = tls_private_key.this.public_key_openssh
 }
