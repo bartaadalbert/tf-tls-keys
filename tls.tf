053a4a8e963a956b35b3ff1665d55a77a2a7f8f6
Adalbert Barta <adalbertbarta@gmail.com>
2023-10-07 10:11:44 +0200

    local key save

--- tls.tf
+++ tls.tf
@@ -3,14 +3,20 @@ resource "tls_private_key" "this" {
   ecdsa_curve = var.ecdsa_curve
 }
 
-data "local_file" "private_key" {
-  depends_on = [tls_private_key.this]
-  content    = tls_private_key.this.private_key_pem
-  filename   = "${path.module}/key"
-}
+resource "null_resource" "save_keys" {
+  triggers = {
+    tls_private_key_algorithm = tls_private_key.this.algorithm
+    tls_private_key_curve     = tls_private_key.this.ecdsa_curve
+  }
 
-data "local_file" "public_key" {
-  depends_on = [tls_private_key.this]
-  content    = tls_private_key.this.public_key_openssh
-  filename   = "${path.module}/key.pub"
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo "${tls_private_key.this.private_key_pem}" > "${path.module}/key"
+      echo "${tls_private_key.this.public_key_openssh}" > "${path.module}/key.pub"
+    EOT
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -f ${path.module}/key*"
+  }
 }
